fa132c557fdb3e5c54c803eafeb10a6049ba8be7
gus4214 <98795239+gus4214@users.noreply.github.com>
2022-06-09 17:34:42 +0900

    Add:hyunbeom upload

--- src/pages/hyunbeom/Login/Login.js
+++ src/pages/hyunbeom/Login/Login.js
@@ -1,7 +1,94 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import "./Login.scss";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
-const LoginHyunBeom = () => {
-  return <div></div>;
-};
+function LoginBox(props) {
+  return (
+    <article className="loginBox ">
+      <header className="loginHeader">
+        <h1 className="loginHeaderLogo logo">Westagram</h1>
+      </header>
+      <form
+        className="loginForm"
+        onSubmit={(event) => {
+          event.preventDefault();
+          props.goToMain(props.loginId, props.passwordId);
+        }}
+      >
+        <input
+          type="text"
+          className="loginFormId"
+          placeholder="전화번호, 사용자 이름 또는 이메일"
+          onChange={(event) => {
+            props.setLoginId(event.target.value);
+          }}
+        />
+        <input
+          type="password"
+          placeholder="비밀번호"
+          className="loginFormPassword"
+          onChange={(event) => {
+            props.setPasswordId(event.target.value);
+          }}
+        />
 
-export default LoginHyunBeom;
+        <button
+          type="submit"
+          className="loginFormBtn"
+          disabled={props.isDisabled}
+        >
+          <span>로그인</span>
+        </button>
+      </form>
+      <div className="loginText">
+        <h3>비밀번호를 잊으셨나요?</h3>
+      </div>
+    </article>
+  );
+}
+
+function Login() {
+  const navigate = useNavigate();
+  const goToMain = (id, password) => {
+    if (id.indexOf("@") >= 0 && password.length >= 5) {
+      navigate("/main");
+    } else {
+      alert("아니야");
+    }
+  };
+
+  const btnHandler = (id, password) => {
+    if (id.indexOf("@") >= 0 && password.length >= 5) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  };
+
+  let [loginId, setLoginId] = useState("");
+  let [passwordId, setPasswordId] = useState("");
+
+  let [isDisabled, setIsDisabled] = useState(true);
+
+  useEffect(() => {
+    if (loginId && passwordId) {
+      btnHandler(loginId, passwordId);
+    }
+  }, [loginId, passwordId]);
+
+  return (
+    <main className="login">
+      <LoginBox
+        loginId={loginId}
+        passwordId={passwordId}
+        setLoginId={setLoginId}
+        setPasswordId={setPasswordId}
+        goToMain={goToMain}
+        isDisabled={isDisabled}
+      />
+    </main>
+  );
+}
+
+export default Login;
--- src/pages/hyunbeom/Main/Main.js
+++ src/pages/hyunbeom/Main/Main.js
@@ -1,7 +1,180 @@
-import React from 'react';
+import React from "react";
+import "./Main.scss";
+import Nav from "../../components/Nav/Nav";
 
-const MainHyunBeom = () => {
-  return <div></div>;
-};
+function Main() {
+  return (
+    <>
+      <Nav></Nav>
 
-export default MainHyunBeom;
+      <main className="mainpage">
+        <div className="feeds">
+          <article className="feeds-article">
+            <div className="feeds-user-name">
+              <img src="/images/myPhoto.jpeg" />
+              <span className="bold">hyunbeom__hi</span>
+              <i className="fa fa-ellipsis"></i>
+            </div>
+            <div className="feeds-pic">
+              <img src="/images/feed.jpeg" />
+            </div>
+            <section className="feeds-like">
+              <div className="feeds-like__icon">
+                <div className="feeds-like__icon-left">
+                  <i className="fa-regular fa-lg fa-heart big-heart"></i>
+                  <i className="fa-regular fa-lg fa-flip-horizontal fa-comment"></i>
+                  <i className="fa fa-lg fa-arrow-up-from-bracket"></i>
+                </div>
+                <div className="feeds-like__icon-right">
+                  <i className="fa-regular fa-lg fa-bookmark"></i>
+                </div>
+              </div>
+              <div className="feeds-like__config">
+                <div className="feeds-like__config-pic"></div>
+                <span>
+                  <span className="bold">aineworld</span>님
+                  <span className="bold">외 10명</span>이 좋아합니다
+                </span>
+              </div>
+              <div className="feeds-like__chat-screen">
+                <div className="feeds-like__chat-mine">
+                  <span className="bold">canon_mj</span>
+                  <span>위워크에서 진행한 도자기 클래스...</span>
+                  <span className="gray">더 보기</span>
+                </div>
+                <div className="feeds-like__chat-friends">
+                  <div className="feeds-like__chat-friends-left">
+                    <span className="bold">neceosecius</span>
+                    <span>이야~~~ 잘 만들었네 </span>
+                  </div>
+                  <div className="feeds-like__chat-friends-right">
+                    <button className="like-button">
+                      <i className="fa fa-x gray hidden"></i>
+                      <i className="fa-regular fa-heart gray"></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <span className="chating-time gray">42분 전</span>
+            <form className="feeds-chating">
+              <input
+                className="feeds-chating-input"
+                type="text"
+                placeholder="댓글 달기..."
+              />
+              <button>게시</button>
+            </form>
+          </article>
+        </div>
+        <div className="right">
+          <div className="main-right">
+            <div className="main-right-user">
+              <div className="user-pic">
+                <img src="/images/wecode.jpeg" />
+              </div>
+              <div className="user-info">
+                <span className="user-info__text bold">wecode_bootcamp</span>
+                <span className="user-info__text gray">WeCode | 위코드</span>
+              </div>
+            </div>
+            <div className="main-right-story">
+              <header className="main-right-story__header">
+                <span className="gray">스토리</span>
+                <span>모두보기</span>
+              </header>
+              <div className="main-right-user">
+                <div className="user-pic">
+                  <img src="/images/wecode.jpeg" />
+                </div>
+                <div className="user-info">
+                  <span className="user-info__text bold">wecode_bootcamp</span>
+                  <span className="user-info__text gray">WeCode | 위코드</span>
+                </div>
+              </div>
+              <div className="main-right-user">
+                <div className="user-pic">
+                  <img src="/images/wecode.jpeg" />
+                </div>
+                <div className="user-info">
+                  <span className="user-info__text bold">wecode_bootcamp</span>
+                  <span className="user-info__text gray">WeCode | 위코드</span>
+                </div>
+              </div>
+              <div className="main-right-user">
+                <div className="user-pic">
+                  <img src="/images/wecode.jpeg" />
+                </div>
+                <div className="user-info">
+                  <span className="user-info__text bold">wecode_bootcamp</span>
+                  <span className="user-info__text gray">WeCode | 위코드</span>
+                </div>
+              </div>
+              <div className="main-right-user">
+                <div className="user-pic">
+                  <img src="/images/wecode.jpeg" />
+                </div>
+                <div className="user-info">
+                  <span className="user-info__text bold">wecode_bootcamp</span>
+                  <span className="user-info__text gray">WeCode | 위코드</span>
+                </div>
+              </div>
+            </div>
+            <div className="main-right-story recommend">
+              <header className="main-right-story__header">
+                <span className="gray">회원님을 위한 추천</span>
+                <span>모두보기</span>
+              </header>
+              <div className="main-right-user">
+                <div className="user-pic">
+                  <img src="/images/wecode.jpeg" />
+                </div>
+                <div className="user-info">
+                  <span className="user-info__text bold">wecode_bootcamp</span>
+                  <span className="user-info__text gray">WeCode | 위코드</span>
+                </div>
+                <div className="user-info-follow">
+                  <span>팔로우</span>
+                </div>
+              </div>
+              <div className="main-right-user">
+                <div className="user-pic">
+                  <img src="/images/wecode.jpeg" />
+                </div>
+                <div className="user-info">
+                  <span className="user-info__text bold">wecode_bootcamp</span>
+                  <span className="user-info__text gray">WeCode | 위코드</span>
+                </div>
+                <div className="user-info-follow">
+                  <span>팔로우</span>
+                </div>
+              </div>
+              <div className="main-right-user">
+                <div className="user-pic">
+                  <img src="/images/wecode.jpeg" />
+                </div>
+                <div className="user-info">
+                  <span className="user-info__text bold">wecode_bootcamp</span>
+                  <span className="user-info__text gray">WeCode | 위코드</span>
+                </div>
+                <div className="user-info-follow">
+                  <span>팔로우</span>
+                </div>
+              </div>
+            </div>
+            <footer>
+              <span className="gray">
+                Instagram 정보 · 지원 · 홍보 센터 · API ·
+                <p>채용 정보 · 개인정보처리방침 · 약관 ·</p>
+                디렉터리 · 프로필 · 해시태그 · 언어
+              </span>
+              <span className="gray"> © 2022 Instagram from Meta</span>
+            </footer>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default Main;
