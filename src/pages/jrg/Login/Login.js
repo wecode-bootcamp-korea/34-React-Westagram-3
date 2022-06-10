05d7c17a18d196d44bd1ca0c7074625773cb8670
dkzks44 <dkzks44@users.noreply.github.com>
2022-06-10 21:52:02 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/jrg/Login/Login.js
+++ src/pages/jrg/Login/Login.js
@@ -1,62 +1,78 @@
 import React from 'react';
-import {Link, Navigate, useNavigate} from 'react-router-dom';
-import "./Login.scss";
-import {useState} from 'react';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
+import './login.scss';
+import { useState } from 'react';
 
 const Login = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-
-    const onSubmit = (e) => {
-        e.preventDefault();
-        if(idValue.indexOf('@')!== -1 && pwValue.length > 5) {
-            navigate("/main")
-        }
+  const onSubmit = e => {
+    e.preventDefault();
+    if (idValue.indexOf('@') !== -1 && pwValue.length > 5) {
+      navigate('/main-jrg');
     }
+  };
 
-    const [idValue,setIdValue] = useState("");
-    const [pwValue,setPwValue] = useState("");
+  const [idValue, setIdValue] = useState('');
+  const [pwValue, setPwValue] = useState('');
 
-    const [classSetting,setClassSetting] = useState(false);
+  const [classSetting, setClassSetting] = useState(false);
 
-    const getId = (event) => {
-        event.preventDefault();
-        setIdValue(event.target.value);
-        if(idValue.indexOf('@')!== -1 && pwValue.length > 5) {
-            setClassSetting(true);
-        } else{
-            setClassSetting(false)
-        }
-        console.log(classSetting);
+  const getId = event => {
+    event.preventDefault();
+    setIdValue(event.target.value);
+    if (idValue.indexOf('@') !== -1 && pwValue.length > 5) {
+      setClassSetting(true);
+    } else {
+      setClassSetting(false);
     }
+    console.log(classSetting);
+  };
 
-    const getPw = (event) => {
-        event.preventDefault();
-        setPwValue(event.target.value);
-        if(idValue.indexOf('@')!== -1 && pwValue.length > 5) {
-            setClassSetting(true)
-        } else {
-            setClassSetting(false)
-        }
-        console.log(classSetting);
+  const getPw = event => {
+    event.preventDefault();
+    setPwValue(event.target.value);
+    if (idValue.indexOf('@') !== -1 && pwValue.length > 5) {
+      setClassSetting(true);
+    } else {
+      setClassSetting(false);
     }
+    console.log(classSetting);
+  };
 
-    return (
-        <div className="body">
-            <div className="wrapDiv">
-                <div className="inLine">
-                    <span className="titleLogin"> Westagram</span>
-                    <form className="loginForm" onSubmit={onSubmit}>
-                        <input className="idInput" type="text" placeholder="전화번호, 사용자 이름 또는 이메일" value={idValue} onChange={getId}/>
-                        <input className="pwInput" type="password" placeholder="비밀번호" value={pwValue} onChange={getPw}/>
-                        <button className={classSetting? "loginBtnActive":"loginBtnUnactive"}>로그인</button>
-                    </form>
-                    <a className="forgotPw" href="">비밀번호를 잊으셨나요?</a>
-                 </div>
-            </div>
+  return (
+    <div className="body">
+      <div className="wrapDiv">
+        <div className="inLine">
+          <span className="titleLogin"> Westagram</span>
+          <form className="loginForm" onSubmit={onSubmit}>
+            <input
+              className="idInput"
+              type="text"
+              placeholder="전화번호, 사용자 이름 또는 이메일"
+              value={idValue}
+              onChange={getId}
+            />
+            <input
+              className="pwInput"
+              type="password"
+              placeholder="비밀번호"
+              value={pwValue}
+              onChange={getPw}
+            />
+            <button
+              className={classSetting ? 'loginBtnActive' : 'loginBtnUnactive'}
+            >
+              로그인
+            </button>
+          </form>
+          <a className="forgotPw" href="">
+            비밀번호를 잊으셨나요?
+          </a>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
-
 
 export default Login;
